adf8736aea92fa45f2bd7b986882056f243d85ac
coyoteboss <50458782+coyoteboss@users.noreply.github.com>
2019-05-13 09:51:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -37,32 +37,42 @@ bot.on('message' , message => {
         }
     }
 
-    if(message.content === 'I Summon Exodia') {
+     message === message.content.toLocaleUpperCase
+
+    if(message.content === 'I SUMMON EXODIA') {
         message.reply("Ah Exodia it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon Exodia Necross') {
+    else if(message.content === 'I SUMMON EXODIA NECROSS') {
         message.reply("Ah Exodia Necross it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon The Legendary Exodia Incarnate') {
+    else if(message.content === 'I SUMMON THE LEGENDARY EXODIA INCARNATE') { 
         message.reply("Ah The Legendary Exodia Incarnate it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon Exodia Master of The Guard') {
+    else if(message.content === 'I SUMMON EXODIA MASTER OF THE GUARD') { 
         message.reply("Ah Exodia Master of The Guard it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon Exxod Master of the Guard') {
+    else if(message.content === 'I SUMMON EXXOD MASTER OF THE GUARD') { 
         message.reply("Ah Exxod Master of the Guard it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon Exodius the Ultimate Forbidden Lord') {
+    else if(message.content === 'I SUMMON EXODIUS THE ULTIMATE FORBIDDEN LORD') { 
         message.reply("Ah Exodius the Ultimate Forbidden Lord it's not possible. Nobody's ever been able to summon him!");
     }
 
-    else if(message.content === 'I Summon Explodia') {
+    else if(message.content === 'I SUMMON EXPLODIA') { 
         message.reply("Ah Explodia it's not possible. Nobody's ever been able to escape him!");
+    }
+
+    else if(message.content === "MY GRANDFATHER'S DECK HAS NO PATHETIC CARDS") {
+        message.reply("Ah Exodia it's not possible. Nobody's ever been able to summon him!");
+    }
+
+    else if(message.content === "DRAW YOUR LAST PATHETIC CARD") {
+        message.reply("My grandfather's deck has no pathetic cards");
     }
 });
 

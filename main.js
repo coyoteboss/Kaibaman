abdd45285b39fb18d22655cada89e897d68d9b5f
coyoteboss <50458782+coyoteboss@users.noreply.github.com>
2020-12-03 18:28:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -235,14 +235,13 @@ bot.on('message' , message => {
         isReady = false;
         message.channel.send({embed: {
 //                 color: 3447003,
-                description: "[I summon myself Kaibaman! ACCTIVATION TEST](https://drive.google.com/open?id=18Q-D7YjhWk8Vy4h1lYfu5RPTRbtoiyWy)"
+                description: "[I summon myself Kaibaman!](https://drive.google.com/open?id=18Q-D7YjhWk8Vy4h1lYfu5RPTRbtoiyWy)"
             }});
         isReady = true;
     }
     
-    else if(isReady && textmsg === "!TestBroadcast") {
+    else if(isReady && textmsg "!testbroadcast") {
         isReady = false;
-        message.reply("made it");
         /*
         const broadcast = client.voice.createBroadcast();
         broadcast.play('./Kaibaman.mp3');
